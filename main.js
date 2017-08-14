8a44bb74578f88951ed1ff9a3bde5514e89856ee
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2017-08-14 17:52:03 +0200

    sortNode: Refactoring in preparation of sliced sort

--- main.js
+++ main.js
@@ -6,21 +6,28 @@ async function sortNode(node, compareFunction) {
     return;
   }
 
-  let sorted = node.children.slice().sort(compareFunction);
-  let moved = 0, len = sorted.length, subtrees = [];
+  let subtrees = [];
 
-  for (let i = 0; i < len; i++) {
-    let n = sorted[i];
+  {
+    let start = 0, items = node.children.slice().sort(compareFunction);
+    let moved = 0, len = items.length;
 
-    if (i !== n.index + moved) {
-      await browser.bookmarks.move(n.id, { "index": i });
-      moved++;
+    for (let i = 0; i < len; i++) {
+      let n = items[i], index = start + i;
+
+      if (index !== n.index + moved) {
+        await browser.bookmarks.move(n.id, { index: index });
+        moved++;
+      }
+
+      if (n.children) subtrees.push(n);
     }
 
-    if (n.children) subtrees.push(n);
+    if (moved) {
+      con.log("Sorted \"%s\", slice %d..%d, %d items moved",
+        node.title || node.id, start, start + len, moved);
+    }
   }
-
-  if (moved) con.log("Sorted \"%s\", %d items moved", node.title || node.id, moved);
 
   await Promise.all(subtrees.map((n) => sortNode(n, compareFunction)));
 }
