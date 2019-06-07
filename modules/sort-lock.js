704263e36782b52e401b959f33dc960a12e9e308
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2019-06-07 22:25:03 +0200

    sortLock: Destructure `sorts` before use

--- modules/sort-lock.js
+++ modules/sort-lock.js
@@ -14,32 +14,34 @@ const sortLock = {
 
     // Wait for a previous sort to complete
     async wait(id) {
-        let promise = this.sorts.get(id);
+        const {sorts} = this;
+        let promise = sorts.get(id);
 
         if (promise) {
             con.log("Waiting on %o: %o", id, promise);
             await promise;
-            promise = this.sorts.get(id);
+            promise = sorts.get(id);
         }
 
         return !!promise;
     },
 
     async run(id, asyncFunc) {
-        if (this.sorts.has(id)) throw new Error(`Already sorting ${id}`);
+        const {sorts} = this;
+        if (sorts.has(id)) throw new Error(`Already sorting ${id}`);
 
         // XXX: Safety valve
-        if (this.sorts.size >= 10000) throw new Error("Too many concurrent sorts");
+        if (sorts.size >= 10000) throw new Error("Too many concurrent sorts");
 
         const promise = asyncFunc();
-        this.sorts.set(id, promise);
+        sorts.set(id, promise);
 
         try {
-            if (this.sorts.size === 1) this.notify();
+            if (sorts.size === 1) this.notify();
             await promise;
         } finally {
-            this.sorts.delete(id);
-            if (this.sorts.size === 0) this.notify();
+            sorts.delete(id);
+            if (sorts.size === 0) this.notify();
         }
     },
 };
